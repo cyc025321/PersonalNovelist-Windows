49860db573116018f5e9753bef1658daccb26579
迎超 崔 <1161510844@qq.com>
2023-07-11 21:21:48 +0800

    添加项目文件。

---
+++ Page/Other/SoftWareAbout.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PersonalNovelist_Windows.Page.Other
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class SoftWareAbout : Window
+    {
+        public SoftWareAbout()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ Services/FileName.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalNovelist_Windows.Services
+{
+
+    public interface ICalculationService
+    {
+        double TipAmount(double subTotal, int generosity);
+    }
+    public class CalculationService : ICalculationService
+    {
+        public double TipAmount(double subTotal, int generosity)
+        {
+            return subTotal * ((double)generosity) / 100.0;
+        }
+    }
+}
